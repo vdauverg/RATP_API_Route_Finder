ba39822b7fab3cf7b60e43b9a60fc5c607930236
Vincent Dauvergne <vdauverg@e1z2r5p26.42.us.org>
2019-10-13 01:04:38 -0700

    getting all lines

--- leroute.py
+++ leroute.py
@@ -1,9 +1,17 @@
 import sys
 import requests
+import json
 
 if len(sys.argv) != 3:
 	print("Usage: executable 'Current Position' 'Destination'")
 	exit()
 
-request = requests.get('https://api.navitia.io/v1/coverage/sandbox/pt_objects?q=etro%200', auth=('3b036afe-0110-4202-b9ed-99718476c2e0', ''))
-print(request.text)
+departure = requests.get('https://api.navitia.io/v1/coverage/fr-idf/physical_modes/physical_mode:Metro/lines', auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
+print(departure.text)
+
+# print("\n--------------- --------------------\n")
+
+# arrival = requests.get('https://api.navitia.io/v1/coverage/fr-idf/pt_objects?q=' + sys.argv[2], auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
+# # print(arrival.text)
+
+# print (json.loads(arrival.text)["pt_objects"])
