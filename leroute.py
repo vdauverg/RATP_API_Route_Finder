d36ed4ad59621ff7d57c37bdd89f3edf2d8dd8ff
Vincent Dauvergne <vdauverg@e1z2r5p29.42.us.org>
2019-10-13 21:17:47 -0700

    most apis calls are made

--- leroute.py
+++ leroute.py
@@ -6,12 +6,29 @@ if len(sys.argv) != 3:
 	print("Usage: executable 'Current Position' 'Destination'")
 	exit()
 
-departure = requests.get('https://api.navitia.io/v1/coverage/fr-idf/physical_modes/physical_mode:Metro/lines', auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
-print(departure.text)
+departure = requests.get('https://api.navitia.io/v1/coverage/fr-idf/pt_objects?q=' + sys.argv[1] + '&disable_geojson=true', auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
+departure = json.loads(departure.text)["pt_objects"]
 
-# print("\n--------------- --------------------\n")
+arrival = requests.get('https://api.navitia.io/v1/coverage/fr-idf/pt_objects?q=' + sys.argv[2] + '&disable_geojson=true', auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
+arrival = json.loads(arrival.text)["pt_objects"]
 
-# arrival = requests.get('https://api.navitia.io/v1/coverage/fr-idf/pt_objects?q=' + sys.argv[2], auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
-# # print(arrival.text)
+lines = requests.get('https://api.navitia.io/v1/coverage/fr-idf/physical_modes/physical_mode:Metro/lines?disable_geojson=true', auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
+lines = json.loads(lines.text)["lines"]
 
-# print (json.loads(arrival.text)["pt_objects"])
+line_ids = []
+for l in lines:
+	line_ids.append(l['id'])
+
+
+start_lines = requests.get('https://api.navitia.io/v1/coverage/fr-idf/stop_areas/' + departure[0]['id'] + '/physical_modes/physical_mode:Metro/lines?disable_geojson=true&disable_disruptions=true', auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
+start_lines = json.loads(start_lines.text)["lines"]
+sl_ids = []
+for line in start_lines:
+	sl_ids.append(line["id"])
+
+end_lines = requests.get('https://api.navitia.io/v1/coverage/fr-idf/stop_areas/' + arrival[0]['id'] + '/physical_modes/physical_mode:Metro/lines?disable_geojson=true&disable_disruptions=true', auth=('74c2531a-4e3e-41d3-8617-d6ed3501dd22', ''))
+end_lines = json.loads(end_lines.text)["lines"]
+el_ids = []
+for line in end_lines:
+	el_ids.append(line["id"])
+
